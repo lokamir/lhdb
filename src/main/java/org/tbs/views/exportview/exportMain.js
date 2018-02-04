8282c60833510eccb6a95c1158178016d1ebdb6f
lokamir <y_lokamir@163.com>
2018-02-04 23:14:09 +0800

    导出模板定稿

--- src/main/java/org/tbs/views/exportview/exportMain.js
+++ src/main/java/org/tbs/views/exportview/exportMain.js
@@ -3,6 +3,7 @@ var postjson= {
 		ureportFileName:"${request.getParameter('ureportFileName')}",//xxxx.ureport.xml
 		beginDateProperty:"${request.getParameter('beginDateProperty')}",//ture or false
 		endDateProperty:"${request.getParameter('endDateProperty')}",//ture or false
+		projNameProperty:"${request.getParameter('projNameProperty')}",//ture or false
 		exportFileName:"${request.getParameter('exportFileName')}",
 		type:"${request.getParameter('type')}"//[1,6]
 };
@@ -10,22 +11,30 @@ var postjson= {
 
 /** @Bind view.onReady */
 !function(self,arg){
-	if(postjson.beginDateProperty){
-		view.get("#formCondition").getElement("beginDate").set("visible",postjson.beginDateProperty);
+	if(postjson.projNameProperty =="false"){
+		view.get("#formCondition").getElement("projname").set("visible",false);
 	}
-	if(postjson.endDateProperty){
-		view.get("#formCondition").getElement("endDate").set("visible",postjson.endDateProperty);
+	if(postjson.beginDateProperty=="false"){
+		view.get("#formCondition").getElement("beginDate").set("visible",false);
 	}
+	if(postjson.endDateProperty=="false"){
+		view.get("#formCondition").getElement("endDate").set("visible",false);
+	}
+	debugger;
 };
 
 
 /** @Bind #preview.onClick */
 !function(self){
 var param = view.get("#formCondition.entity");
+var proj_name = param.get("projname");
 var bd = param.get("beginDate");
 var ed = param.get("endDate");
 var beginDate;
 var endDate;
+if(!proj_name){
+	proj_name = '';
+}
 if(bd){
 	beginDate = bd.getFullYear() + '-' + (bd.getMonth() + 1) + '-' + bd.getDate()  ;
 }else{
@@ -34,7 +43,7 @@ if(bd){
 if(ed){
 	endDate = ed.getFullYear() + '-' + (ed.getMonth() + 1) + '-' + ed.getDate()  ;
 }else{
-	endDate = '9999-12-31';
+	endDate = '2999-12-31';
 }
 var subViewHolderExport = view.get("#subViewHolderExport");
 //subViewHolderExport.set("path","./Export_Daily.jsp?bdate="+beginDate+"&edate="+endDate);
@@ -42,6 +51,7 @@ subViewHolderExport.set("path",
 	"./ureport/preview?_t="+postjson.type
 	+"&_u=file:"+postjson.ureportFileName
 	+"&_n="+postjson.exportFileName
+	+"&proj_name="+proj_name
 	+"&beginDate="+beginDate+"&endDate="+endDate);
 };
 
